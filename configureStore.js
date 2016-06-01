e75196fcef2a7bcf8fe6114778a3b3c52f8ba97c
Sen Wang <wangsen@orgrimmar.io>
2016-06-01 16:47:11 +0800

    add redux-promise module

--- configureStore.js
+++ configureStore.js
@@ -1,11 +1,12 @@
 import rootReducer from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, promiseMiddleware)
   );
 }
